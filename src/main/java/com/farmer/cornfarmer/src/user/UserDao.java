bb6ad8450459fedd929f23b7417b60059669f3b3
HONGMINJU <mhong0800@gmail.com>
2022-02-10 22:02:55 +0900

    :bug: inactive params오류 수정

--- src/main/java/com/farmer/cornfarmer/src/user/UserDao.java
+++ src/main/java/com/farmer/cornfarmer/src/user/UserDao.java
@@ -217,8 +217,8 @@ public class UserDao {
     }
 
     public int inactive(int userIdx) {
-        String inactiveQuery = "update user set active = ? , oauth_id = ?  where user_idx=?";
-        Object[] inactiveParams = new Object[]{false, "", userIdx};
+        String inactiveQuery = "update user set active = ? , oauth_id = ?, nickname = ?   where user_idx=?";
+        Object[] inactiveParams = new Object[]{false, "", "", userIdx};
         this.jdbcTemplate.update(inactiveQuery, inactiveParams);
 
         return userIdx;
