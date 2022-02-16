913931964756553da579590b7d7fd03f5f6f01ee
wheoghks6 <wheoghks0309@gmail.com>
2022-02-16 19:52:05 +0900

    :sparkles: 기본이미지 설정

--- src/main/java/com/farmer/cornfarmer/src/user/UserController.java
+++ src/main/java/com/farmer/cornfarmer/src/user/UserController.java
@@ -10,14 +10,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.farmer.cornfarmer.utils.S3Uploader;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Objects;
 
 @RestController
 @RequestMapping("/users")
-public class UserController {
 
+public class UserController {
+    final String default_Img = "https://cornfarmer.s3.ap-northeast-2.amazonaws.com/user/cornfarmerProfile.PNG";
     final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
@@ -121,14 +123,25 @@ public class UserController {
         try {
             //kakao naver.
             String ouath_id = jwtService.getOauthId();
+            String PhotoUrl = "";
             if(userProvider.checkExistOauthId(ouath_id) == false)
-            {
+            { //oauth_id 확인
                 throw new BaseException(BaseResponseStatus.DATABASE_ERROR);
             }
+
             if (userProvider.duplicateNick(postUserReq.getNickname())) {
+                //닉네임 중복확인
                 throw new BaseException(BaseResponseStatus.DUPLICATE_NICKNAME);
             }
-            String PhotoUrl = S3Uploader.upload(postUserReq.getPhoto(), "user");
+
+            if(postUserReq.getPhoto().isEmpty())
+            { //회원가입시 프로필 사진설정 없다면 기본 이미지로 지정
+                PhotoUrl = default_Img.toString();
+            }
+            else {
+                //사진이미지 존재한다면 해당 사진업로드
+                PhotoUrl = S3Uploader.upload(postUserReq.getPhoto(), "user");
+            }
             PostUserRes postUserRes = userService.createUserInfo(postUserReq, PhotoUrl, ouath_id);
             return new BaseResponse<>(postUserRes);
         } catch (BaseException exception) {
@@ -185,18 +198,33 @@ public class UserController {
     public BaseResponse<PostLoginRes> modifyMyInfo(@PathVariable int userIdx, @ModelAttribute PostUserInfoReq postUserInfoReq){
         try{
             int tokenIdx = jwtService.getUserIdx();
+
+            String PhotoUrl = "";
             if(userIdx == tokenIdx && !(userIdx == 0)){
                 if(userProvider.checkDuplicateNick(postUserInfoReq.getNickname(), userIdx))
-                {
+                { //닉네임 중복확인
                     throw new BaseException(BaseResponseStatus.DUPLICATE_NICKNAME);
                 }
                 //이전에 저장되어있던 사진파일 삭제
                 String currentPhoto = userProvider.getCurrentUserPhoto(userIdx);
-                currentPhoto = currentPhoto.replace("https://cornfarmer.s3.ap-northeast-2.amazonaws.com/", "");
-                if(S3Uploader.isPhotoExist(currentPhoto)) {
-                    S3Uploader.delete(currentPhoto);
+
+                if(!Objects.equals(currentPhoto,default_Img))
+                { //이전에 사진이 기본이미지가 아니라면
+                    //이전에 존재하는 사진 삭제
+                    currentPhoto = currentPhoto.replace("https://cornfarmer.s3.ap-northeast-2.amazonaws.com/", "");
+                    if (S3Uploader.isPhotoExist(currentPhoto)) {
+                        S3Uploader.delete(currentPhoto);
+                    }
                 }
-                String PhotoUrl = S3Uploader.upload(postUserInfoReq.getPhoto(), "user");
+
+                if(postUserInfoReq.getPhoto().isEmpty())
+                { //기본이미지로 변경할 경우
+                    PhotoUrl = default_Img.toString();
+                }
+                else {
+                    //다른이미지로 변경한 경우
+                    PhotoUrl = S3Uploader.upload(postUserInfoReq.getPhoto(), "user");
+                }
                 return new BaseResponse<>(userService.modifyMyInfo(userIdx, postUserInfoReq, PhotoUrl));
             } else {
                 return new BaseResponse<>(BaseResponseStatus.INVALID_USER_JWT);
