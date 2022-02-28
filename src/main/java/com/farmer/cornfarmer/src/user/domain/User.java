4c0eeb049a31112a57e66a9bd10b3e818636cc5e
HONGMINJU <mhong0800@gmail.com>
2022-02-28 22:08:47 +0900

    :art: UserSocialType enum 생성

---
+++ src/main/java/com/farmer/cornfarmer/src/user/domain/User.java
@@ -0,0 +1,22 @@
+package com.farmer.cornfarmer.src.user.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userIdx;
+
+
+}
