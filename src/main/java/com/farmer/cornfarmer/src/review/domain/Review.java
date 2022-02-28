4b01c27753a8fa2ab491fb26908d457128e105d1
HONGMINJU <mhong0800@gmail.com>
2022-03-01 02:55:30 +0900

    :art: review와 user, movie연결

--- src/main/java/com/farmer/cornfarmer/src/review/domain/Review.java
+++ src/main/java/com/farmer/cornfarmer/src/review/domain/Review.java
@@ -1,6 +1,8 @@
 package com.farmer.cornfarmer.src.review.domain;
 
 import com.farmer.cornfarmer.src.common.domain.BaseTimeEntity;
+import com.farmer.cornfarmer.src.movie.domain.Movie;
+import com.farmer.cornfarmer.src.user.domain.User;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,10 +14,24 @@ import javax.persistence.*;
 @Entity
 public class Review extends BaseTimeEntity {
 
+    //TODO : likecnt 지우고 싶어요.........!!!!!!!!(그래서 안했어요!)
+    //TODO : active 지우고 싶어요.........!!!!!!!!(그래서 안했어요!)
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long reviewIdx;
 
     @Column(nullable = false)
     private String contents;
+
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+    @JoinColumn(name="movie_idx")
+    private Movie movie;
+
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+    @JoinColumn(name="user_idx")
+    private User writer;
+
+    private float rate;
+
 }
